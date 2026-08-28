5dbc7b9c0014f3b01923b3c30f8b72c3073150db
Esqi-Meazza <sqmznapwi@gmail.com>
2026-08-28 23:30:37 +0700

    feat: refactor routing structure by moving routes to AppRoutes component

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// peserta
-import LandingPage from "./components/public/landingPage.jsx";
-import DoorPrize from "./components/public/doorprize.jsx";
-import Grit from "./components/public/features/boxgrid.jsx"
-// panitia 
-import Login from "./components/admin/login.jsx";
-import ProtectedRoute from "./components/admin/protectedRoute.jsx";
-import AdminLayout from "./components/admin/dashboardAdmin.jsx";
-import DashboardPage from "./components/admin/pages/dashboard.jsx";
-import MainEventPage from "./components/admin/pages/mainEvent.jsx";
-import PesertaPage from "./components/admin/pages/peserta.jsx";
-import DoorprizePage from "./components/admin/pages/doorprize.jsx";
-import SettingPage from "./components/admin/pages/setting.jsx";
-import ProjectorDisplay from "./components/admin/pages/projectorDisplay.jsx";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes.jsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* PESERTA */}
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/doorprize" element={<DoorPrize />} />
-        <Route path="/grid" element={<Grit />} />
-
-        {/* PANITIA */}
-        <Route path="/admin" element={<Login />} />
-        <Route element={<ProtectedRoute />}>
-        <Route element={<AdminLayout />}>
-          {/* HALAMAN PANITIA */}
-          <Route path="/admin/dashboard" element={<DashboardPage />} />
-          <Route path="/admin/main" element={<MainEventPage />} />
-          <Route path="/admin/peserta" element={<PesertaPage />} />
-          <Route path="/admin/doorprize" element={<DoorprizePage />} />
-          <Route path="/admin/setting" element={<SettingPage />} />
-          <Route path="/admin/projector" element={<ProjectorDisplay />} />
-
-        </Route>
-      </Route>
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,43 @@
+import { Routes, Route } from "react-router-dom";
+// peserta
+import LandingPage from "../components/public/LandingPage.jsx";
+import DoorPrize from "../components/public/Doorprize.jsx";
+// universal
+import Grid from "../components/public/features/boxgrid.jsx";
+// panitia
+import ProtectedRoute from "./ProtectedRoute.jsx";
+import Login from "../components/admin/Login.jsx";
+import AdminLayout from "../components/admin/DashboardAdmin.jsx";
+import DashboardPage from "../components/admin/pages/Dashboard.jsx";
+import MainEventPage from "../components/admin/pages/MainEvent.jsx";
+import PesertaPage from "../components/admin/pages/Peserta.jsx";
+import DoorprizePage from "../components/admin/pages/Doorprize.jsx";
+import SettingPage from "../components/admin/pages/Setting.jsx";
+import ProjectorDisplay from "../components/admin/pages/ProjectorDisplay.jsx";
+
+function AppRoutes() {
+	return (
+		<Routes>
+			{/* PESERTA */}
+			<Route path="/" element={<LandingPage />} />
+			<Route path="/doorprize" element={<DoorPrize />} />
+			<Route path="/grid" element={<Grid />} />
+
+			{/* PANITIA */}
+			<Route path="/admin" element={<Login />} />
+			<Route element={<ProtectedRoute />}>
+				<Route element={<AdminLayout />}>
+					{/* HALAMAN PANITIA */}
+					<Route path="/admin/dashboard" element={<DashboardPage />} />
+					<Route path="/admin/main" element={<MainEventPage />} />
+					<Route path="/admin/peserta" element={<PesertaPage />} />
+					<Route path="/admin/doorprize" element={<DoorprizePage />} />
+					<Route path="/admin/setting" element={<SettingPage />} />
+					<Route path="/admin/projector" element={<ProjectorDisplay />} />
+				</Route>
+			</Route>
+		</Routes>
+	);
+}
+
+export default AppRoutes;
